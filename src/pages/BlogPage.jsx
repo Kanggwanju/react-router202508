92d05a138f1ac932a84e72edb96ae736060181f2
Kanggwanju <xprtktm1@gmail.com>
2025-08-29 14:30:53 +0900

    14. 카테고리 클릭 이벤트

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -4,7 +4,6 @@ import {posts} from '../dummy-data/dummy-post.js';
 import PostCard from '../components/PostCard.jsx';
 import {useSearchParams} from 'react-router-dom';
 import BlogFilter from '../components/BlogFilter.jsx';
-import BlogSideBar from '../components/BlogSideBar.jsx';
 
 const BlogPage = () => {
 
@@ -33,7 +32,7 @@ const BlogPage = () => {
       <div className={styles.blog}>
 
         <BlogFilter />
-        
+
         <div className={styles.grid}>
           {posts
             // 제목 또는 내용으로 검색
